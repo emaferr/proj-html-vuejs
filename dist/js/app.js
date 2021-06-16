1c70efe9bc62217bd4aa2ce5e4c2503fd00860aa
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-16 15:52:05 +0200

    Incorporate VueJs Use it to dynamically cycle services

--- dist/js/app.js
+++ dist/js/app.js
@@ -3057,6 +3057,24 @@ function within(min, value, max) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var bootstrap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.esm.js");
 
+var app = new Vue({
+  el: '#app',
+  data: {
+    services: [{
+      image_service: 'avadabarbers-trimcut-icon-before',
+      name_service: 'Trim & Cut',
+      description_service: 'Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.'
+    }, {
+      image_service: 'avadabarbers-washndry-icon',
+      name_service: 'Wash & Dry',
+      description_service: 'Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.'
+    }, {
+      image_service: 'avadabarbers-beardtrim-icon',
+      name_service: 'Beard Tidy',
+      description_service: 'Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.'
+    }]
+  }
+});
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
 import 'bootstrap';
+
+const app = new Vue({
+
+    el:'#app',
+
+    data:{
+
+        services: [
+
+            {
+                image_service: 'avadabarbers-trimcut-icon-before',
+                name_service:'Trim & Cut',
+                description_service:'Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.'
+            },
+            {
+                image_service: 'avadabarbers-washndry-icon',
+                name_service:'Wash & Dry',
+                description_service:'Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.'
+            },
+            {
+                image_service: 'avadabarbers-beardtrim-icon',
+                name_service:'Beard Tidy',
+                description_service:'Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.'
+            }
+        ]
+
+    },
+    
+    
+})
