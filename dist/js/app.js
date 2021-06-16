8a07bef7d2810df9c656ad23068e81c350671991
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-16 16:05:46 +0200

    use VueJs to cycle products dynamically

--- dist/js/app.js
+++ dist/js/app.js
@@ -3072,6 +3072,23 @@ var app = new Vue({
       image_service: 'avadabarbers-beardtrim-icon',
       name_service: 'Beard Tidy',
       description_service: 'Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.'
+    }],
+    products: [{
+      image_product: 'brush_dark.png',
+      name_product: 'Brush',
+      price_product: '15.00'
+    }, {
+      image_product: 'scissors.webp',
+      name_product: 'Scissors',
+      price_product: '85.00'
+    }, {
+      image_product: 'hot_oil_dark.png',
+      name_product: 'Hot Oil',
+      price_product: '15.00'
+    }, {
+      image_product: 'straight_razor_dark.png',
+      name_product: 'Straight Razor',
+      price_product: '30.00'
     }]
   }
 });
