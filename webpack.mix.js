f956764bd4df3331b250e8df18e04d522fa23b58
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-16 12:51:25 +0200

    Correct path background images

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,6 @@ let mix = require('laravel-mix');
 
 mix
 .js('src/app.js', 'js')
-.sass('src/app.scss', 'css')
+.sass('src/app.scss', 'css').options({ processCssUrls: false })
+.copyDirectory('src/img', 'dist/img')
 .setPublicPath('dist');
